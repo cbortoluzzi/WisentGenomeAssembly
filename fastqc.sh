88f25fb666202b8f27bfa421ac0027093c1a649c
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2023-07-25 16:20:03 +0200

    Update fastqc.sh

--- fastqc.sh
+++ fastqc.sh
@@ -15,17 +15,17 @@
 
 if [ $# -ne 4 ]
 then
-    	echo -e "\nusage: `basename $0` <sample_name> <fastq_1> <fastq_2> <output_directory>\n"
+    	echo -e "\nusage: `basename $0` <sample name> <paired-end run R1 in FASTQ format> <paired-end run R2 in FASTQ format> <output directory>\n"
         echo -e "DESCRIPTION: This script takes a pair of fastq files and outputs a QC report\n\n"
 
-        echo -e "INPUT:         <sample_name>           Code or name to identify the sample"
-        echo -e "               <fastq_1>               First paired end FASTQ file"
-        echo -e "               <fastq_2>              	Second paired end FASTQ file"
-        echo -e "               <output_directory>      Name of output directory\n\n"
+        echo -e "INPUT:         <sample name>                         Code or name to identify the sample"
+        echo -e "               <paired-end run R1 in FASTQ format>   First paired end FASTQ file"
+        echo -e "               <paired-end run R2 in FASTQ format>   Second paired end FASTQ file"
+        echo -e "               <output directory>                    Name of output directory\n\n"
 
-        echo -e "OUTPUT:        <a report, one per pair of fastq files> The report will be stored in the output directory in a directory defined by the sample name\n\n"
-
-        echo -e "REQUIRES:      Loading of FastQC (v0.11.9) module\n\n"
+        echo -e "OUTPUT:        <a report, one per paired-end run>\n\n"
+        
+        echo -e "REQUIRES:      Requires FastQC (v0.11.9) module available from PATH\n\n"
 
         exit
 fi
@@ -38,17 +38,17 @@ fastq_2=$3
 outDir=$4
 
 
-# load module
+# Load module
 module load fastqc/0.11.9
 
 
 mkdir -p $outDir/fastqc
 
-# The first thing we need to do is to run FastQC - a high throughput sequence QC analysis tool
-# The idea here is to produce for each fastq file a quality control report that will be used to check for potential problems in the data
+# This command line runs FastQC, a high throughput sequence QC analysis tool
+# The idea here is to produce for each paired-end run in FASTQ format a report that will be used to check for potential problems in the data
 fastqc $fastq_1 $fastq_2 -o $outDir/fastqc --extract -f fastq -d /tmp/
 
-# We will also run fastq-stat as this program outputs some interesting statistics
+# We will also run fastq-stat as this program outputs some additional statistics that might be of interest
 fastq-stat --sampleid $sample --result $outDir/fastqc/$sample.csv --plot $fastq_1 $fastq_2
 
 
