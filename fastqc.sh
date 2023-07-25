3a1a68d7f50e100a62449a347704cf6af2c0e718
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2023-07-25 16:48:21 +0200

    Update fastqc.sh

--- fastqc.sh
+++ fastqc.sh
@@ -15,12 +15,12 @@
 
 if [ $# -ne 4 ]
 then
-    	echo -e "\nusage: `basename $0` <sample name> <paired-end run R1 in FASTQ format> <paired-end run R2 in FASTQ format> <output directory>\n"
+    	echo -e "\nusage: `basename $0` <sample name> <paired-end R1 in FASTQ format> <paired-end R2 in FASTQ format> <output directory>\n"
         echo -e "DESCRIPTION: This script takes a pair of fastq files and outputs a QC report\n\n"
 
         echo -e "INPUT:         <sample name>                         Code or name to identify the sample"
-        echo -e "               <paired-end run R1 in FASTQ format>   First paired end FASTQ file"
-        echo -e "               <paired-end run R2 in FASTQ format>   Second paired end FASTQ file"
+        echo -e "               <paired-end R1 in FASTQ format>   First paired end FASTQ file"
+        echo -e "               <paired-end R2 in FASTQ format>   Second paired end FASTQ file"
         echo -e "               <output directory>                    Name of output directory\n\n"
 
         echo -e "OUTPUT:        <two reports, one per paired-end run, and one for both paired-end runs combined>\n\n"
