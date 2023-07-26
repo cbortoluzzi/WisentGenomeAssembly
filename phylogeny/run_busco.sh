204652576d8f8c5c6ae94313d413752cc3d014a3
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2023-07-26 16:43:37 +0200

    Update run_busco.sh

--- phylogeny/run_busco.sh
+++ phylogeny/run_busco.sh
@@ -22,7 +22,7 @@ then
 
         echo -e "OUTPUT:        <completeness score in BUSCO>           A folder with BUSCO completeness score results\n\n"
 
-        echo -e "REQUIRES:  Activation of BUSCO conda environment\n\n"
+        echo -e "REQUIRES:      Activation of BUSCO conda environment\n\n"
 
         exit
 fi
@@ -32,7 +32,7 @@ genome=$1
 lineage=$2
 
 
-outDir=$(basename $genome | sed 's/.fasta//g' |  sed 's/.fa/.busco/g')
+outDir=$(basename $genome | sed 's/.fasta/.busco/g' |  sed 's/.fa/.busco/g')
 
 # Run BUSCO with the metaeuk mode
 busco -i $genome -o $outDir -m genome -l $lineage --offline -c 10
