9c5972c84b202d6e003f7b0f56de1081214f5e83
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2023-07-26 14:16:22 +0200

    Update repeatmasker.sh

--- repeat_annotation/repeatmasker.sh
+++ repeat_annotation/repeatmasker.sh
@@ -16,29 +16,29 @@
 
 if [ $# -ne 2 ]
 then
-    	echo -e "\nusage: `basename $0` <de-novo_repeat_library.fasta> <reference-genome.fasta>\n"
+    	echo -e "\nusage: `basename $0` <a de novo repeat library> <reference genome in FASTA format>\n"
         echo -e "DESCRIPTION: This script runs RepeatMasker using as input the de-novo repeat library built from LTR retriever and RepeatModeler\n\n"
 
-        echo -e "INPUT:         <de-novo_repeat_library.fasta>  De-novo repeat library built from LTR retriever and RepeatModeler\n"
-        echo -e "INPUT:         <reference-genome.fasta>        Reference genome assembly in FASTA format\n\n"
+        echo -e "INPUT:         <a de novo repeat library>            De-novo repeat library built from RepeatModeler\n"
+        echo -e "INPUT:         <reference genome in FASTA format>    Reference genome assembly in FASTA format\n\n"
 
         echo -e "OUTPUT:        <a masked reference genome assembly in FASTA format>\n\n"
 
-        echo -e "REQUIRES:      Installation of RepeatMasker (v4.1.5) accessible from PATH\n\n"
+        echo -e "REQUIRES:      Requires RepeatMasker (v4.1.5) accessible from PATH\n\n"
 
         exit
 fi
 
 
-# Export path to RepeatMasker
+
 export PATH=/cluster/work/pausch/cbortoluzzi/softwares/RepeatMasker:$PATH
 
-
-de_novo_repeat_library=$1
-reference_genome=$2
+library=$1
+ref=$2
 
 
 # Run RepeatMasker
-outDir=$(dirname $de_novo_repeat_library)
+outDir=$(dirname $library)
 mkdir -p $outDir/RepeatMasker
-RepeatMasker -e rmblast -pa 10 -s -lib $de_novo_repeat_library -dir $outDir/RepeatMasker -a -xsmall -gff $reference_genome
+RepeatMasker -e rmblast -pa 10 -s -lib $library -dir $outDir/RepeatMasker -a -xsmall -gff $ref
+
