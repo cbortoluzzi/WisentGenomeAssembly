173c5ee5961962e51c5cca8197448ce0f1abfdbe
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2023-07-27 10:52:38 +0200

    Update de_novo_repeat_library.sh

--- repeat_annotation/de_novo_repeat_library.sh
+++ repeat_annotation/de_novo_repeat_library.sh
@@ -12,46 +12,45 @@
 #SBATCH --error=error_%J
 
 
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
 then
-	echo -e "\nusage: `basename $0` <reference genome in FASTA format> <output directory>\n"
+	echo -e "\nusage: `basename $0` <reference-genome.fasta> <output directory>\n"
 	echo -e "DESCRIPTION: This script takes a reference genome assembly in FASTA format and builds a de novo repeat library from scratch with RepeatModeler\n\n"
 
-	echo -e "INPUT:          <reference genome in FASTA format>      Reference genome assembly in FASTA format"
-	echo -e "                <output directory>                      Name of output directory\n\n"
- 	
-  	echo -e "OUTPUT:        <a RepeatModeler BLAST database>"
-	echo -e "               <a de novo repeat library>"
+	echo -e "INPUT:		<reference-genome.fasta>	Reference genome assembly in FASTA format\n\n"
 
-	echo -e "REQUIRES:      Requires RepeatModeler (v2.0.4), samtools (v1.15.1) and Genometools (v1.6.2) accessible from PATH\n\n"
+	echo -e "OUTPUT:	<a RepeatModeler BLAST database>"
+	echo -e "		<a de novo repeat library>"
+
+	echo -e "REQUIRES:  Installation of RepeatModeler (v2.0.4), samtools (v1.15.1) and Genometools (v1.6.2) accessible from PATH\n\n"
 
 	exit
 fi
+
+
 
 
 reference=$1
 output_directory=$2
 
 
-species_name=$(dirname $reference | rev | cut -f 1 -d '/' | rev)
-outDir=`echo $output_directory'/'$species_name`
+name=$(basename $reference | sed 's/.fasta//g' | sed 's/.fa//g' | sed 's/.fasta.gz//g' | sed 's/.fa.gz//g')
 
 
-mkdir -p $outDir
+mkdir -p repeat_annotation
 
 
 # Prepare reference-genome.fasta
-unmasked=$(basename $reference | sed 's/.fasta.gz//g' | sed 's/.fa.gz//g')".rm.unmasked.fasta"
-python3 /cluster/work/pausch/cbortoluzzi/bin/repeat_annotation_pipeline/prepare_fasta.py --i $reference --o $outDir
+unmasked=$name.rm.unmasked.fasta
+python3 /cluster/work/pausch/cbortoluzzi/bin/repeat_annotation_pipeline/prepare_fasta.py --i $reference --o repeat_annotation
 
 
 # Build RepeatModeler BLAST database
 name=$(basename $unmasked .fasta)
-mkdir -p $outDir/RepeatModeler
-cd $outDir/RepeatModeler
+cd repeat_annotation
 if [[ ! -f $name.nhr ]]; then
 	echo -e "BLAST database not found. Building it...this might take some time\n\n"
-	BuildDatabase -name $name -engine ncbi -dir $outDir
+	BuildDatabase -name $name -engine ncbi -dir .
 fi
 
 
@@ -60,4 +59,5 @@ if [[ ! -f "RM_*/consensi.fa" ]]; then
 	echo -e "De novo repeat identification not found. Making it now...this might take a few days\n\n"
 	RepeatModeler -threads 16 -database $name
 fi
+
 
