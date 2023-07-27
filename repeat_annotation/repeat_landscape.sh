df9b34eefee2108cdbb4f5a4bd0a5a9a4fb6bfae
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2023-07-27 10:56:36 +0200

    Update repeat_landscape.sh

--- repeat_annotation/repeat_landscape.sh
+++ repeat_annotation/repeat_landscape.sh
@@ -19,9 +19,11 @@ lib=$2
 
 name=$(basename $aln | sed 's/.fasta.align//g' | sed 's/.fa.align//g')
 
+mkdir -p repeat_annotation
+
 # Calculate Kimura divergence corrected for GC content
-calcDivergenceFromAlign.pl -s $name.divsum -a $name.new.align $aln
+calcDivergenceFromAlign.pl -s repeat_annotation/$name.divsum -a repeat_annotation/$name.new.align $aln
 
 # Create a repeat landscape graph using the divergence summary data generated with the calcDivergenceFromAlign.pl script; 2564834688 is the genome size of the B. bonasus
-createRepeatLandscape.pl -div $name.divsum -g 2564834688 > $name.html
+createRepeatLandscape.pl -div repeat_annotation/$name.divsum -g 2564834688 > repeat_annotation/$name.html
 
