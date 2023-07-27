4e8c0a1fca905b4439fca66a2a6255d35716c9ec
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2023-07-27 11:05:17 +0200

    Create busco2phylo.sh

---
+++ phylogeny/busco2phylo.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+
+# Author : cbortoluzzi@ethz.ch
+
+
+#SBATCH -n 8
+#SBATCH --time=5-24:00:00
+#SBATCH --mem-per-cpu=4000
+#SBATCH --job-name=busco2phylo
+#SBATCH --output=output_%J
+#SBATCH --error=error_%J
+
+
+if [ $# -ne 1 ]
+then
+	echo -e "\nusage: `basename $0` <list of assemblies>\n"
+	echo -e "DESCRIPTION: This script builds a phylogenetic tree from a set of complete, single copy BUSCO genes\n\n"
+
+	echo -e "INPUT:         <list of assemblies>     A text file with the name of all assemblies to include in the phylogenetic tree, one per line\n\n"
+ 
+	echo -e "OUTPUT:        <a supermatrix>          A supermatrix containing the aa sequence of all complete, single copy BUSCO genes\n"
+  echo -e "               <a phylogenetic tree>    A phylogenetic tree obtained from RAxML after 1,000 bootstraps\n\n"
+
+	echo -e "REQUIRES:      Requires mafft (v7.490), trimAl (), and RAxML () available from PATH\n\n"
+
+  exit
+fi
+
+
+# Export paths
+export PATH=/cluster/work/pausch/cbortoluzzi/softwares/mafft-7.490-with-extensions/bin:$PATH
+export PATH=/cluster/work/pausch/cbortoluzzi/softwares/trimal/source:$PATH
+export PATH=/cluster/work/pausch/cbortoluzzi/softwares/standard-RAxML:$PATH
+
+
+
+list_assembly=$1
+
+
+num_genomes=`wc -l $list_assembly | awk '{print $1}'`
+
+cat $list_assembly | while read species; do cat /cluster/work/pausch/cbortoluzzi/wisent_project/assemblies/$species/mammalia_odb10/run_mammalia_odb10/full_table.tsv | grep -v '^#' | awk '$2 == "Complete"{print $1}' >> complete_busco_ids.txt;done
+sort complete_busco_ids.txt | uniq -c | awk '$1=="'$num_genomes'"{print $2}' > final_busco_ids.txt
+
+mkdir -p $outDir && mkdir -p $outDir/mafft && mkdir -p $outDir/trimal
+
+cat final_busco_ids.txt | while read busco_id;do
+	mkdir -p $outDir/fasta/$busco_id
+	cat $list_species | while read species;do
+		for faa in /cluster/work/pausch/cbortoluzzi/wisent_project/assemblies/$species/mammalia_odb10/run_mammalia_odb10/busco_sequences/single_copy_busco_sequences/$busco_id.faa
+		do
+			cat $faa | awk '/^>/{print ">'$species'"; next}{print}' > $outDir/fasta/$busco_id/$species.$busco_id.fa
+		done
+	cat $outDir/fasta/$busco_id/*.fa >> $outDir/mafft/$busco_id.aln
+	# Align protein sequences with MAFFT
+	mafft --amino $outDir/mafft/$busco_id.aln > $outDir/mafft/$busco_id.aln.mafft
+	rm $outDir/mafft/$busco_id.aln
+	# Trim alignments with trimAL
+	trimal -in $outDir/mafft/$busco_id.aln.mafft -out $outDir/trimal/$busco_id.aln.mafft.trimal -gt 0.8 -st 0.001
+done
+
+
+# Generate supermatrix
+mkdir -p $outDir/matrix
+python3 superalignment.py --i $outDir/trimal --o $outDir/matrix
+
+
+# Run Raxml with 1,000 bootstraps
+raxmlHPC-SSE3 -T 16 -f a -m PROTGAMMAJTT -N 1000 -n my_busco_phylo -s $outDir/matrix/supermatrix.aln.mafft.trimal.fa -p 13432 -x 89090
+
