072ff9fd87ee6e822ad5dfed2883d00bd11ea642
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2023-07-27 11:20:57 +0200

    Update de_novo_repeat_library.sh

--- repeat_annotation/de_novo_repeat_library.sh
+++ repeat_annotation/de_novo_repeat_library.sh
@@ -14,23 +14,23 @@
 
 if [ $# -ne 1 ]
 then
-	echo -e "\nusage: `basename $0` <reference-genome.fasta> <output directory>\n"
-	echo -e "DESCRIPTION: This script takes a reference genome assembly in FASTA format and builds a de novo repeat library from scratch with RepeatModeler\n\n"
+    	echo -e "\nusage: `basename $0` <reference genome in FASTA format>\n"
+        echo -e "DESCRIPTION: This script takes a reference genome assembly in FASTA format and builds a de novo repeat library from scratch with RepeatModeler\n\n"
 
-	echo -e "INPUT:		<reference-genome.fasta>	Reference genome assembly in FASTA format\n\n"
+        echo -e "INPUT:         <reference genome in FASTA format>      Reference genome assembly in FASTA format\n\n"
 
-	echo -e "OUTPUT:	<a RepeatModeler BLAST database>"
-	echo -e "		<a de novo repeat library>"
+        echo -e "OUTPUT:        <a de novo repeat library>      	A species-specific de novo repeat library\n\n"
 
-	echo -e "REQUIRES:  Installation of RepeatModeler (v2.0.4), samtools (v1.15.1) and Genometools (v1.6.2) accessible from PATH\n\n"
+        echo -e "REQUIRES:      Requires RepeatModeler (v2.0.4), samtools (v1.15.1) and Genometools (v1.6.2) accessible from PATH\n\n"
 
-	exit
+        exit
 fi
 
 
 
 
 reference=$1
+output_directory=$2
 
 
 name=$(basename $reference | sed 's/.fasta//g' | sed 's/.fa//g' | sed 's/.fasta.gz//g' | sed 's/.fa.gz//g')
@@ -48,15 +48,14 @@ python3 /cluster/work/pausch/cbortoluzzi/bin/repeat_annotation_pipeline/prepare_
 name=$(basename $unmasked .fasta)
 cd repeat_annotation
 if [[ ! -f $name.nhr ]]; then
-	echo -e "BLAST database not found. Building it...this might take some time\n\n"
-	BuildDatabase -name $name -engine ncbi -dir .
+        echo -e "BLAST database not found. Building it...this might take some time\n\n"
+        BuildDatabase -name $name -engine ncbi -dir .
 fi
 
 
 # Run RepeatModeler
 if [[ ! -f "RM_*/consensi.fa" ]]; then
-	echo -e "De novo repeat identification not found. Making it now...this might take a few days\n\n"
-	RepeatModeler -threads 16 -database $name
+        echo -e "De novo repeat identification not found. Making it now...this might take a few days\n\n"
+        RepeatModeler -threads 16 -database $name
 fi
-
 
