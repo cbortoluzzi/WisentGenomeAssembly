40e3bd1b9205fdecc173c38f2650bac15b9a5d7b
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2024-02-26 13:32:17 +0100

    Update pca.sh

--- principal_component_analysis/pca.sh
+++ principal_component_analysis/pca.sh
@@ -13,7 +13,7 @@
 
 if [ $# -ne 1 ]
 then
-    	echo -e "\nusage: `basename $0` <VCF>\n"
+        echo -e "\nusage: `basename $0` <VCF>\n"
         echo -e "DESCRIPTION: This script generates a principal component analysis on a set of individuals whose SNPs are filtered for linkage disequilibrium and MAF\n\n"
 
         echo -e "INPUT:           <VCF>        A VCF input file (all chromosomes)\n\n"
@@ -21,7 +21,7 @@ then
         echo -e "OUTPUT:          <A VCF file without the water buffalo sample>"
         echo -e "                 <a PCA output file with eigenval and eigenvec>   \n\n"
 
-        echo -e "REQUIRES:       Requires VCFTools (v0.1.16) and Plink (v1.90b6.18) available from PATH\n\n"
+        echo -e "REQUIRES:       Requires VCftools (v0.1.16) and plink (v1.90b6.18) available from PATH\n\n"
 
         exit
 fi
@@ -32,14 +32,25 @@ fi
 module load plink
 module load vcftools
 
+
 vcf=$1
+
 
 # Generate a principal componenet analysis in PLINK
 # We need to add --double-id because PLINK has some difficulties in parsing samples ID that contain '_'
 # We also need to add --allow-extra-chr to allow all the chromosomes to be included in the PCA analysis (PLINK is set to work on human data)
-mkdir -p pca
 echo -e "Filter out the water buffalo sample\n\n"
-vcftools --gzvcf $vcf --remove-indv Water_buffalo --recode --recode-INFO-all --stdout | bgzip -c > $name.noWaterBuffalo.vcf.gz
+mkdir -p pca
+vcftools --gzvcf $vcf --remove-indv Water_buffalo --plink --out pca/chromosome.all.fgeno.qual.rm.biallelic.noindels.noWaterBuffalo
+
 
 echo -e "Generate a principal component analysis on a set of bi-allelic SNPs present in all samples and filtered for LD and a MAF < 5%\n\n"
-plink --vcf $name.noWaterBuffalo.vcf.gz --pca --double-id --chr-set 29 --allow-extra-chr --threads 10 --indep-pairwise 50 10 0.2 --maf 0.05 --geno 0 --recode vcf --out pca/PCA.noWaterBuffalo
+plink --file pca/chromosome.all.fgeno.qual.rm.biallelic.noindels.noWaterBuffalo --double-id --chr-set 29 --allow-extra-chr --threads 10 --indep-pairwise 50 10 0.2 --maf 0.05 --geno 0 --out pca/PCA.noWaterBuffalo --recode
+plink --file pca/PCA.noWaterBuffalo --double-id --chr-set 29 --allow-extra-chr --threads 10 --exclude pca/PCA.noWaterBuffalo.prune.out --recode --out pca/input.pca
+plink --file pca/input.pca --double-id --chr-set 29 --allow-extra-chr --threads 10 --pca --out pca/input.pca
+
+
+echo -e "Plot principal component analysis\n\n"
+Rscript plot_pca.r pca/input.pca.eigenvec pca/input.pca.eigenval > pca/log
+
+
