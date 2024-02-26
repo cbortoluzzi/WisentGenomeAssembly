0ddbb54925a5ed6a295faeb324d78a7b5dbc50a0
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2024-02-26 13:20:36 +0100

    Update admixture.sh

--- Admixture/admixture.sh
+++ Admixture/admixture.sh
@@ -16,13 +16,13 @@
 if [ $# -ne 1 ]
 then
                 echo -e "\nusage: `basename $0` <VCF>\n"
-                echo -e "DESCRIPTION: This script runs the ADMIXTURE analysis to estimate individual ancestries by efficiently computing maximum likelihood estimates in a parametric model\n\n"
+                echo -e "DESCRIPTION: This script runs the admixture analysis to estimate individual ancestries by efficiently computing maximum likelihood estimates in a parametric model\n\n"
 
-                echo -e "INPUT:           <VCF>                 A VCF input file (all chromosomes)\n\n"
+                echo -e "INPUT:           <VCF>                 A VCF input file that contains only bi-allelic SNPs (all chromosomes)\n\n"
 
                 echo -e "OUTPUT:          <Individual ancestries as estimated by ADMIXTURE>\n\n"
 
-                echo -e "REQUIRES:       Requires Plink (v1.90b6.18), Admixture (v1.3.0), and VCFtools (0.1.16) available from PATH\n\n"
+                echo -e "REQUIRES:        Requires Plink (v1.90b6.18), Admixture (v1.3.0), and VCFtools (0.1.16) available from PATH\n\n"
 
                 exit
 fi
@@ -35,11 +35,12 @@ export PATH=/path/to/admixture_linux-1.3.0:$PATH
 module load plink
 module load vcftools
 
-# The VCF file contains only bi-allelic SNPs
+
+
 vcf=$1
 
 
-# Prepare input for ADMIXTURE: the program requires unlinked (i.e. LD pruned) SNPs in plink format
+# Prepare input for Admixture: the program requires unlinked (i.e. LD pruned) SNPs in plink format
 echo -e "Prepare input for ADMIXTURE: remove water buffalo sample and filter bi-allelic SNPs for linkage disequilibrium"
 mkdir -p admixture
 zcat $vcf | head -n 1000 | grep '^#' | tail -n1 | cut -f 10- | tr '\t' '\n' > admixture/samples.list
@@ -50,7 +51,8 @@ vcftools --gzvcf $vcf --plink --out admixture/input
 plink --file admixture/input --make-bed --chr-set 29 --allow-extra-chr --double-id --keep admixture/samples.keep --recode --indep-pairwise 50 10 0.2 --geno 0.3 --maf 0.05 --out admixture/input
 plink --bfile admixture/input --double-id --chr-set 29 --allow-extra-chr --threads 10 --exclude admixture/input.prune.out --make-bed --out admixture/input.admixture
 
-for i in $(seq 2 10);do admixture --cv=10 -B1000 admixture/input.admixture.bed $i | tee admixture/log.$i.out;done
+for i in $(seq 1 10);do admixture --cv=10 -B1000 admixture/input.admixture.bed $i | tee admixture/log.$i.out;done
 
-# ADMIXTURE produces 2 outputs: .Q which contains cluster assignments for each individual, and .P which containts for each SNP the population allele frequencies
+# Admixture produces 2 outputs: .Q which contains cluster assignments for each individual, and .P which containts for each SNP the population allele frequencies
 awk '/CV/ {print $3,$4}' admixture/*out | cut -c 4,7-20 > admixture/cv.error
+
