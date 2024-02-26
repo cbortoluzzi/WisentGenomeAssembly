0d337688716b5fa0e43bba663f72c04e5b9ae935
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2024-02-26 13:38:49 +0100

    Update treemix.sh

--- treemix/treemix.sh
+++ treemix/treemix.sh
@@ -12,6 +12,24 @@
 #SBATCH --error=error_%J
 
 
+
+if [ $# -ne 1 ]
+then
+                echo -e "\nusage: `basename $0` <VCF>\n"
+                echo -e "DESCRIPTION: This script runs the treemix analysis that, given a set of allele frequencies from a number of populations, it will return the maximum likelihood tree for the set of populations, and optionally attempt to infer a number of admixture events.\n\n"
+
+                echo -e "INPUT:           <VCF>                 A VCF input file that contains only bi-allelic SNPs (all chromosomes)\n\n"
+
+                echo -e "OUTPUT:          <A maximum likelihood tree for the set of populations>\n\n"
+
+                echo -e "REQUIRES:        Requires plink (v1.90b6.18), BCFtools (v1.16), and VCFtools (0.1.16) available from PATH\n\n"
+
+                exit
+fi
+
+
+
+
 vcf=$1
 
 
@@ -27,16 +45,20 @@ vcftools --gzvcf $vcf --plink --out input
 plink --file input --make-bed --chr-set 29 --allow-extra-chr --double-id --indep-pairwise 50 10 0.2 --geno 0 --out input
 plink --bfile input --make-bed --chr-set 29 --allow-extra-chr --double-id --exclude input.prune.out --out input.treemix
 
+
 # Generate cluster file
 echo -e "Generate cluster sample file\n\n"
 bcftools query -l $vcf |  awk '{split($1,pop,"."); print $1"\t"$1"\t"$1}' > samples.clust
+
 
 # Create input for Treemix
 echo -e "Create input file for Treemix\n\n"
 plink --bfile input.treemix --freq --missing --within samples.clust --out input.treemix --chr-set 29 -allow-extra-chr
 gzip input.treemix.frq.strat
 
+
 python2.7 plink2treemix.py input.treemix.frq.strat.gz input.treemix.frq.gz
+
 
 # Run Treemix
 echo -e "Run Treemix\n\n"
